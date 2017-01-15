9063aef84dbceead78033610925dce007e1f77a4
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-01-15 18:09:01 -0200

    Fixes plant not showing until collision

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -25,10 +25,13 @@ public abstract class Entity {
 	
 	public Entity(int maxAnimations, Transform transform) {
 		this.animations = new Animation[maxAnimations];
-		
-		this.transform = transform;
-		this.useAnimation = 0;
-		
+        this.useAnimation = 0;
+
+        this.transform = new Transform();
+        this.transform.pos.set(transform.pos.x,
+                transform.pos.y, 0);
+        this.transform.scale.set(transform.scale.x, transform.scale.y, transform.scale.z);
+
 		this.isSolid = false;
 		
 		boundingBox = new AABB(
@@ -50,6 +53,41 @@ public abstract class Entity {
 		boundingBox.getCenter().set(transform.pos.x, transform.pos.y);
 	}
 	
+	public abstract void update(float delta, Window window, Camera camera, World world);
+	
+	public void render(Shader shader, Camera camera, World world) {
+		Matrix4f target = camera.getProjection();
+		target.mul(world.getWorldMatrix());
+		
+		shader.bind();
+		shader.setUniform("sampler", 0);
+		shader.setUniform("projection", transform.getProjection(target));
+		animations[useAnimation].bind(0);
+		
+		model.render();
+	}
+	
+	public static void initAsset() {
+		float[] vertices = new float[] {
+				-1f, 1f, 0, // 0
+				1f, 1f, 0,  // 1
+				1f, -1f, 0, // 2
+				-1f, -1f, 0,// 3
+			};
+			
+		float[] texture = new float[] {0,0, 1,0, 1,1, 0,1};
+		int[] indexes = new int[] {
+			0, 1, 2,
+			2, 3, 0
+		};
+		
+		model = new Model(vertices, texture, indexes);
+	}
+	
+	public static void deleteAsset() {
+		model = null;
+	}
+
 	public void collideWithTiles(World world) {
 		AABB[] boxes = new AABB[25];
 		
@@ -108,41 +146,6 @@ public abstract class Entity {
 		}
 	}
 	
-	public abstract void update(float delta, Window window, Camera camera, World world);
-	
-	public void render(Shader shader, Camera camera, World world) {
-		Matrix4f target = camera.getProjection();
-		target.mul(world.getWorldMatrix());
-		
-		shader.bind();
-		shader.setUniform("sampler", 0);
-		shader.setUniform("projection", transform.getProjection(target));
-		animations[useAnimation].bind(0);
-		
-		model.render();
-	}
-	
-	public static void initAsset() {
-		float[] vertices = new float[] {
-				-1f, 1f, 0, // 0
-				1f, 1f, 0,  // 1
-				1f, -1f, 0, // 2
-				-1f, -1f, 0,// 3
-			};
-			
-		float[] texture = new float[] {0,0, 1,0, 1,1, 0,1};
-		int[] indexes = new int[] {
-			0, 1, 2,
-			2, 3, 0
-		};
-		
-		model = new Model(vertices, texture, indexes);
-	}
-	
-	public static void deleteAsset() {
-		model = null;
-	}
-
 	public void collideWithEntitiy(Entity entity) {
 		Collision collision = boundingBox.getCollision(entity.boundingBox);
 		
@@ -150,9 +153,6 @@ public abstract class Entity {
 			if (entity.isSolid()) {
 				boundingBox.correctPosition(entity.boundingBox, collision);
 				transform.pos.set(boundingBox.getCenter().x, boundingBox.getCenter().y, 0);
-				
-				entity.transform.pos.set(entity.boundingBox.getCenter().x, 
-						entity.boundingBox.getCenter().y, 0);
 			} else {
 				collision.distance.x /= 2;
 				collision.distance.y /= 2;
