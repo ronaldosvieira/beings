acd02fbaf9fe3e5fffb68bc60e2b244a8e500655
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-03-11 20:16:06 -0300

    Added mind loop in living things update call

--- src/entity/model/LivingThing.java
+++ src/entity/model/LivingThing.java
@@ -2,7 +2,10 @@ package entity.model;
 
 import entity.model.mind.Mind;
 import entity.model.mind.Sense;
+import io.Window;
 import org.joml.Vector2f;
+import render.Camera;
+import world.World;
 
 import java.util.List;
 
@@ -18,6 +21,11 @@ public abstract class LivingThing extends Thing {
 		this.mind = new Mind(this);
 		this.senses = senses;
 	}
+
+    @Override
+    public void update(float delta, Window window, Camera camera, World world) {
+        this.mind.update();
+    }
 
     public List<Sense> getSenses() {return this.senses;}
 	public boolean isAlive() {return this.isAlive;}
