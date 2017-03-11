4824ed0e0fe70789b8124474ed3efd8644565eaf
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-03-11 20:29:59 -0300

    Very basic sense interpretation by Mind

--- src/entity/model/mind/Mind.java
+++ src/entity/model/mind/Mind.java
@@ -8,15 +8,30 @@ import java.util.List;
 
 public class Mind {
     private LivingThing being;
+    private List<Thing> workingMemory;
 
     public Mind(LivingThing being) {
         this.being = being;
+        this.workingMemory = new ArrayList<>();
     }
 
     public void update() {
         List<Thing> perceptions = new ArrayList<>();
         for (Sense sense : this.being.getSenses()) {
             perceptions.addAll(sense.perceive());
+        }
+
+        for (Thing perception : perceptions) {
+            if (!workingMemory.contains(perception)) {
+                workingMemory.add(perception);
+
+                System.out.println("Living thing '" + this.being.getName()
+                        + "' just saw '" + perception.getName() + "'.");
+
+                while (workingMemory.size() > 4) {
+                    workingMemory.remove(0);
+                }
+            }
         }
 
         // todo: do stuff
