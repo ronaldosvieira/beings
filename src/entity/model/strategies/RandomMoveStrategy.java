da8d57bfdc97e1bb69d5ae574d022883478a0bec
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-04-12 10:11:32 -0300

    Adds more randomness on RandomMoveStrategy

--- src/entity/model/strategies/RandomMoveStrategy.java
+++ src/entity/model/strategies/RandomMoveStrategy.java
@@ -9,6 +9,7 @@ import entity.model.Animal;
 public class RandomMoveStrategy extends MoveStrategy {
 	Vector2f direction;
 	float lastMove;
+	float moveTime;
 	boolean isMoving;
 	
 	public RandomMoveStrategy(Animal animal) {
@@ -17,14 +18,16 @@ public class RandomMoveStrategy extends MoveStrategy {
 		this.direction = getRandomDirection();
 		this.isMoving = false;
 		this.lastMove = 0.0f;
-	}
+		this.moveTime = getRandomMoveTime();
+    }
 
 	@Override
 	public Vector2f getMovement(float delta) {
 		lastMove += delta;
-		
-		if (lastMove >= 0.5f) {
+
+        if (lastMove >= moveTime) {
 			lastMove = 0.0f;
+            moveTime = getRandomMoveTime();
 			isMoving = !isMoving;
 			direction = getRandomDirection();
 		}
@@ -41,4 +44,11 @@ public class RandomMoveStrategy extends MoveStrategy {
 		
 		return dir.normalize();
 	}
+
+    private float getRandomMoveTime() {
+	    float base = 0.5f;
+	    float modifier = (new Random().nextFloat() * 0.2f) - 0.1f;
+
+	    return base + modifier;
+    }
 }
