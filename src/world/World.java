75ab867df14c38ff74f591eaa8065ec708c29495
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-04-23 12:24:17 -0300

    Fixes entities rendering sort function

--- src/world/World.java
+++ src/world/World.java
@@ -138,8 +138,14 @@ public class World {
 
         List<Entity> sortedEntities = new ArrayList<>(entities);
 
-        sortedEntities.sort((Entity o1, Entity o2) ->
-                (int) (o2.getPosition().y - o1.getPosition().y));
+        sortedEntities.sort((Entity o1, Entity o2) -> {
+            if (o1.getPosition().y > o2.getPosition().y)
+                return -1;
+            else if (o1.getPosition().y < o2.getPosition().y)
+                return 1;
+            else
+                return 0;
+        });
 
 		List<Entity> walkableEntities = new ArrayList<>(sortedEntities)
                 .parallelStream()
