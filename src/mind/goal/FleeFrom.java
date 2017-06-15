20853048d28092afa04c7fdd7dccfa7d794942ca
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-06-15 15:27:07 -0300

    Changes flee-from goal distance

--- src/mind/goal/FleeFrom.java
+++ src/mind/goal/FleeFrom.java
@@ -7,7 +7,7 @@ import org.joml.Vector2f;
 import java.util.List;
 
 public class FleeFrom extends Goal {
-    private double distance = 10;
+    private double distance = 15;
 
     public FleeFrom(Animal animal) {
         super(animal);
@@ -21,6 +21,8 @@ public class FleeFrom extends Goal {
 
     @Override
     public void cycle(List<TemporalPerception> workingMemory) {
+        if (perception == null) return;
+
         direction = perception
                 .get("distance", Vector2f.class)
                 .negate(new Vector2f())
